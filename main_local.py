72dc23e7ef5ff33ec6d3115f67bce85670deeeed
thongct <bimbimchu20@gmail.com>
2023-10-06 17:48:49 +0700

    push

--- main_local.py
+++ main_local.py
@@ -1,6 +1,6 @@
 
 from models.models import User,Address 
-from models.crud import Insert,Update,Delete,Query,select
+from models.crud import select,delete,insert
 from models.crud import MockSQLSession
 
 session = MockSQLSession()
@@ -22,7 +22,7 @@ patrick = User(name="patrick", fullname="Patrick Star")
 
 # Logic_name: test add, add_all, filte_by,update, commit 
 session.add_all([spongebob, sandy, patrick])
-print(session.query(User).filter_by(name = "spongebob").update({"fullname" : "Super Spongebob Squarepants"}).get_data()) 
+print(session.query(User).filter_by(name = "spongebob").update({"fullname" : "Super Spongebob Squarepants"}).data) 
 print(session)
 session.commit()
 print(session)
@@ -30,4 +30,21 @@ print(session)
 # Logic_name: test select
 print(select(User).where(User.name == 'spongebob'))
 
+print(session.execute(insert(User),[User(name='spongebob_thongchu', fullname='Super Spongebob Squarepants')]))
+session.commit()
+print(session)
+print("\n-----------------------------------------------:",)
 
+print(session.execute(delete(User).where(User.id == 1)))
+print(session)
+print("\n-----------------------------------------------:",)
+
+print(session.execute(select(User)).scalars())
+print(session)
+print("\n-----------------------------------------------:",)
+
+
+# self.db.execute(insert(BacktestResultDetail), [i.model_dump() for i in trades])
+# self.db.execute(delete(BacktestResultDetail)
+# .where(BacktestResultDetail.backtest_results_id == result_id))
+# self.db.execute(select(BacktestResult).where(BacktestResult.strategy_id == strategy_id)).scalar_one
--- models/crud.py
+++ models/crud.py
@@ -4,22 +4,26 @@ storage = {
 buffer = {
     }
 
+class PRINT_YOUR_SELF:
+    def __init__(self):
+        ...
+    def __str__(self):
+        attributes_str = ""
+        for attr, value in self.__dict__.items():
+            attributes_str += f"{attr}: {value}, "
+        return f"CLASS: ({self.__class__.__name__}) : ({attributes_str[:-2]})"
     
-class Query:
-    def __init__(self,data: list,type:str = None):
-        self.__data = data
+class Query(PRINT_YOUR_SELF):
+    def __init__(self,data: list = [],table_schema =None,query_type:str = None):
+        self.data = data
+        self.table_schema = table_schema
+        self.query_type = query_type
 
-    def get_data(self):
-        return self.__data
-
-    def set_data(self, new_data):
-        self.__data = new_data
-    
     def filter_by(self,**kwargs: any):
         print("Filter by" , kwargs)
 
         data_after_filter = []
-        for data in self.__data:
+        for data in self.data:
             checking_filter = True
             for key, value in kwargs.items():
                 if hasattr(data, key) and  getattr(data,key) != value:
@@ -27,50 +31,52 @@ class Query:
             if checking_filter:
                 data_after_filter.append(data)
         
-        return Query(data_after_filter)
+        return Query(data= data_after_filter,table_schema=self.table_schema,query_type='filter_by')
     
     def update(self,values:dict):
         print("update" , values)
 
-        for data in self.__data:
+        for data in self.data:
             for key, value in values.items():
                 if hasattr(data, key) :
                     setattr(data,key,value)
         return self
-    
-    
-
-class Execute:
-    def __init__(self,table_schema : object, query_type = None):
-        self.table_schema = table_schema
-        self.query_type = query_type
     
     def where(self,query):
         data_after_where = []
         left_where_operator = query.left.key 
         operator = query.operator
         right_where_operator = query.right.effective_value
-        for data in self.__data:
+        for data in self.data:
             if  operator(getattr(data, left_where_operator),right_where_operator):
                 data_after_where.append(data) 
         print("\nwhere:",data_after_where)
         
-        return Query(data_after_where)
+        return Query(data= data_after_where,table_schema=self.table_schema,query_type=self.query_type)
+
+    def scalars(self):
+        if(self.query_type == 'SELECT'):
+            return self.data
+    
+    def scalar_one(self):
+        if(self.query_type == 'SELECT'):
+            return self.data[0]
+
+
+def convert_query_class(table_name, query_type):
+    return Query(data = storage[table_name], query_type=query_type, table_schema=table_name)
 
 def insert(table_schema):
-    return Execute(table_schema,"INSERT")
+    return convert_query_class(table_schema.__tablename__,'INSERT')
 
 def update(table_schema):
-    return Execute(table_schema,"UPDATE")
+    return convert_query_class(table_schema.__tablename__,'UPDATE')
 
 def delete(table_schema):
-    return Execute(table_schema,"DELETE")
-
+    return convert_query_class(table_schema.__tablename__,'DELETE')
 
 def select(table_schema):
-    table_name = table_schema.__tablename__
-    print("\nselect" , table_name)
-    return(Query(storage[table_name]))
+    return convert_query_class(table_schema.__tablename__,'SELECT')
 
 class MockSQLSession:
     def __init__(self) -> None:
@@ -114,17 +120,26 @@ class MockSQLSession:
         table_data = []
         if table_name in buffer:
             table_data = buffer[table_name] 
-        return Query(table_data)
+        return Query(data=table_data,table_schema=table_name,query_type='query')
     
-    def execute(self, statement, params):
-        
-        return ...
-    
-    def scalars(self, data: any):
-        return data 
-    
-    def scalar_one(self, data: any):
-        return data 
+    def execute(self, statement, params = None):
+        if statement.query_type == 'INSERT':
+            self.add_all(params)
+        elif statement.query_type == 'DELETE':
+            table_schema = statement.table_schema
+            delete_record = statement.data[0]
+            for record_in_table in storage[table_schema]: 
+                if record_in_table == delete_record:
+                    print('fine data')
+                    storage[table_schema].remove(record_in_table)
+        elif statement.query_type == 'UPDATE':
+            # Logic_name: PENDING
+            ...
+        elif statement.query_type == 'SELECT':
+            return Query(**statement.__dict__)
+        else:
+            print("\nCan't note execute this statement :",statement,params)
+        return 
     
     def __str__(self):
         return f"\nbuffer: {buffer}\nstorage: {storage}" 
